0bd6c888f8d4666657e601bfcd9d99ea94da5ad5
Moritz02 <m.moeller02@gmx.de>
2025-04-09 15:43:50 +0100

    Added code for osm use (not in use)

--- app/src/main/java/hu/matusz/travelapp/MainActivity.java
+++ app/src/main/java/hu/matusz/travelapp/MainActivity.java
@@ -1,12 +1,18 @@
 package hu.matusz.travelapp;
 
+import org.osmdroid.config.Configuration;
+
+import android.content.Context;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,4 +27,24 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
     }
+
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//
+//        Context ctx = getApplicationContext();
+//
+//        // Speicherpfad auf internen Speicher setzen
+//        File osmdroidBasePath = new File(ctx.getFilesDir(), "osmdroid");
+//        if (!osmdroidBasePath.exists()) {
+//            osmdroidBasePath.mkdirs();
+//        }
+//
+//        Configuration.getInstance().setOsmdroidBasePath(osmdroidBasePath);
+//        Configuration.getInstance().setOsmdroidTileCache(new File(osmdroidBasePath, "tiles"));
+//        Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
+//
+//        setContentView(R.layout.activity_main);
+//    }
+
 }
